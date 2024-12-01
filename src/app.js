deeb133dac701b592802bec360abf94dcf5d29e7
Bhatti <jordon5611@gmail.com>
2024-12-02 01:29:17 +0500

    Bugs Solved

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 const app = express();
 
